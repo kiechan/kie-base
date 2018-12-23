92a082d5eecf3ee6ee26539397f79d7611a4437a
koji <koji19880910@gmail.com>
2018-12-23 22:31:28 +0900

    Create sample_satori.js

---
+++ sample_satori.js
@@ -0,0 +1,36 @@
+const satori = require('./utils/satori')
+
+satori.checkAnalyzedWords([
+      { word: '東京',
+        type: '名詞',
+        typeDetail1: '固有名詞',
+        typeDetail2: '地域',
+        typeDetail3: '一般',
+        reading: 'ヨコヤマ' }
+      // { word: '町',
+      //   type: '名詞',
+      //   typeDetail1: '接尾',
+      //   typeDetail2: '地域',
+      //   typeDetail3: null,
+      //   reading: 'マチ' },
+      // { word: '人口',
+      //   type: '名詞',
+      //   typeDetail1: '一般',
+      //   typeDetail2: null,
+      //   typeDetail3: null,
+      //   reading: 'ジンコウ' },
+      // { word: '何',
+      //   type: '名詞',
+      //   typeDetail1: '数',
+      //   typeDetail2: null,
+      //   typeDetail3: null,
+      //   reading: 'ナン' },
+      // { word: '人',
+      //   type: '名詞',
+      //   typeDetail1: '接尾',
+      //   typeDetail2: '助数詞',
+      //   typeDetail3: null,
+      //   reading: 'ニン' }
+]).then(function(res) {
+    console.log(res)
+})
