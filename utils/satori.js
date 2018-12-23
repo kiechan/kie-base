2dcf86cbc4ada97c40a5324cd77897bc600fe1ca
望月由登 <mochidzukiyutou@mochidzukiyutounoMacBook-puro.local>
2018-12-24 04:19:51 +0900

    SATORI修正

--- utils/satori.js
+++ utils/satori.js
@@ -1,6 +1,10 @@
 "use strict"
 
 module.exports.checkAnalyzedWords = (analyzedWords) => {
+
+    return new Promise((resolve, reject) => {
+
+
         // if (analyzedWords == null) {
         //     // 検索条件が無効な場合
         //     console.log('検索条件が無効です。');
@@ -23,7 +27,7 @@ module.exports.checkAnalyzedWords = (analyzedWords) => {
         //  地域検索結果が二か所以上あってもOK
 
 
-        const {Client} = require('pg');
+        const { Client } = require('pg');
         const client = new Client({
             user: 'root',
             host: 'udc2019-aws-db.ctdjivyul3eu.ap-northeast-1.rds.amazonaws.com',
@@ -32,7 +36,7 @@ module.exports.checkAnalyzedWords = (analyzedWords) => {
             port: 5432
         });
         // 接続チェック
-        client.connect(function(err, res) {
+        client.connect((err, res) => {
             if (err) {
                 console.log("DB接続に失敗しました")
                 console.log(err)
@@ -40,194 +44,99 @@ module.exports.checkAnalyzedWords = (analyzedWords) => {
             }
         })
 
-        let fncAry = new Array();
-        let results = new Array();
-        let result = null;
-        let isArea = false;
-        let isKeyword = false;
-        let area = null;
-        let keyword = null;
-        let word = null;
-        let type = null;
-        let typeDetail1 = null;
-        let typeDetail2 = null;
-        let typeDetail3 = null;
-        let reading = null;
-        let queryStr1 =             "SELECT ";
-        queryStr1     = queryStr1 +     "COUNT(mst.*) AS cnt ";
-        queryStr1     = queryStr1 +   "FROM m_area mst ";
-        queryStr1     = queryStr1 +  "WHERE mst.prefectures LIKE $1 ";
-        queryStr1     = queryStr1 +     "OR mst.municipality LIKE $1 ";
-        queryStr1     = queryStr1 +     "OR mst.district LIKE $1 ";
-        let queryStr2 =             "SELECT ";
-        queryStr2     = queryStr2 +     "COUNT(mst.*) AS cnt ";
-        queryStr2     = queryStr2 +   "FROM m_category mst ";
-        queryStr2     = queryStr2 +  "WHERE mst.category_name LIKE $1 ";
-        analyzedWords.forEach(analyzedWord => {
-          result = new Object();
-          isArea = false;
-          isKeyword = false;
-          area = null;
-          keyword = null;
-          word = analyzedWord.word;
-          type = analyzedWord.type;
-          typeDetail1 = analyzedWord.typeDetail1;
-          typeDetail2 = analyzedWord.typeDetail2;
-          typeDetail3 = analyzedWord.typeDetail3;
-          reading = analyzedWord.reading;
-            var fnc = new Promise(function(resolve, reject) {
-              console.log('1dayo');
+        let fncAry = []
 
-              // 地域判定
-              if (type == '地域'
-                  || typeDetail1 == '地域'
-                  || typeDetail2 == '地域'
-                  || typeDetail3 == '地域') {
+        for (let i = 0; i < analyzedWords.length; i++) {
+            fncAry.push(new Promise((resolve, reject) => {
+                let analyzedWord = analyzedWords[i]
 
-                  isArea = true;
-              }
-              // resolve();
-            }).then(new Promise(function(resolve, reject){
-              console.log('2dayo');
+                let result = new Object()
+                let isArea = false
+                let isKeyword = false
+                let area = null
+                let keyword = null
+                let word = analyzedWord.word
+                let type = analyzedWord.type
+                let typeDetail1 = analyzedWord.typeDetail1
+                let typeDetail2 = analyzedWord.typeDetail2
+                let typeDetail3 = analyzedWord.typeDetail3
 
-              client.query(queryStr1, ["%" + word + "%"]).then(res => {
-                  if (res.rows.length > 0) {
-                      if (res.rows[0].cnt != null && res.rows[0].cnt > 0) {
-                          // 地域データが存在する場合
-                          isArea = true;
-                      }
-                  }
-              }).catch(e => {
-                  console.log("データ取得に失敗しました");
-                  console.error(e);
-                  reject();
-              })
-            })).then(new Promise(function(resolve, reject){
-              console.log('3dayo');
-              // キーワード判定
+                // 地域判定
+                if (type == '地域'
+                    || typeDetail1 == '地域'
+                    || typeDetail2 == '地域'
+                    || typeDetail3 == '地域') {
 
-              client.query(queryStr2, ["%" + word + "%"]).then(res => {
-                  if (res.cnt > 0) {
-                      if (res.rows[0].cnt != null && res.rows[0].cnt > 0) {
-                          // カテゴリデータが存在する場合
-                          isKeyword = true;
-                      }
-                  }
-                  if (isArea) {
-                      // 単語が地域の場合
-                      area = word;
-                  }
+                    isArea = true;
+                }
 
-                  if (isKeyword) {
-                      // キーワードが地域の場合
-                      keyword = word;
-                  }
-                  result.isArea = isArea;
-                  result.isKeyword = isKeyword;
-                  result.area = area;
-                  result.keyword = keyword;
-                  results.push(result);
-                  console.log(result);
-                  resolve(result);
-              }).catch(e => {
-                  console.log("データ取得に失敗しました");
-                  console.error(e);
-                  reject();
-              })
-            }).catch(function(err){console.log(err);reject();}));
+                // 地域判定
+                const QUERY_1 =
+                    "SELECT "
+                    + "COUNT(mst.*) AS cnt "
+                    + "FROM m_area mst "
+                    + "WHERE mst.prefectures LIKE $1 "
+                    + "OR mst.municipality LIKE $1 "
+                    + "OR mst.district LIKE $1 "
+                client.query(QUERY_1, ["%" + word + "%"]).then(res => {
+                    console.log('1ok')
+                    if (res.rows.length > 0) {
+                        if (res.rows[0].cnt != null && res.rows[0].cnt > 0) {
+                            // 地域データが存在する場合
+                            isArea = true
+                        }
+                    }
 
-            fncAry.push(fnc);
+                    // キーワード判定
+                    const QUERY_2 =
+                        "SELECT "
+                        + "COUNT(mst.*) AS cnt "
+                        + "FROM m_category mst "
+                        + "WHERE mst.category_name LIKE $1 "
+                    client.query(QUERY_2, ["%" + word + "%"]).then(res => {
+                        console.log('2ok')
+                        if (res.cnt > 0) {
+                            if (res.rows[0].cnt != null && res.rows[0].cnt > 0) {
+                                // カテゴリデータが存在する場合
+                                isKeyword = true
+                            }
+                        }
+                        if (isArea) {
+                            // 単語が地域の場合
+                            area = word
+                        }
+
+                        if (isKeyword) {
+                            // キーワードが地域の場合
+                            keyword = word
+                        }
+
+                        result.isArea = isArea
+                        result.isKeyword = isKeyword
+                        result.area = area
+                        result.keyword = keyword
+
+                        resolve(result)
+                    }).catch(e => {
+                        console.log('2err')
+                        reject(e)
+                    })
+                }).catch(e => {
+                    console.log('1err')
+                    reject(e)
+                })
+            })
+            )
+        }
+
+        Promise.all(fncAry).then(results => {
+            console.log('-----')
+            console.log(results)
+            console.log('-----')
+            client.end()
+        }).catch(err => {
+            console.log(err)
+            client.end()
         });
-
-        return Promise.all(fncAry).then(function(xxx){
-            console.log('ああああああああああああああああああああああ');
-            client.end();
-            //return results;
-        });
-
-
-//client.end();
-//resolve(result);
-
-    // 返却はオブジェクトに配列を2つ（地域配列、キーワード配列）
-}
-
-function clientEnd(client) {
-  client.end();
-}
-
-function checkAnalyzedWord(analyzedWord, client) {
-  let result = new Object();
-  let isArea = false;
-  let isKeyword = false;
-  let area = null;
-  let keyword = null;
-  let word = analyzedWord.word;
-  let type = analyzedWord.type;
-  let typeDetail1 = analyzedWord.typeDetail1;
-  let typeDetail2 = analyzedWord.typeDetail2;
-  let typeDetail3 = analyzedWord.typeDetail3;
-  let reading = analyzedWord.reading;
-
-  // 地域判定
-  if (type == '地域'
-      || typeDetail1 == '地域'
-      || typeDetail2 == '地域'
-      || typeDetail3 == '地域') {
-
-      isArea = true;
-  }
-
-  let queryStr1 =             "SELECT ";
-  queryStr1     = queryStr1 +     "COUNT(mst.*) AS cnt ";
-  queryStr1     = queryStr1 +   "FROM m_area mst ";
-  queryStr1     = queryStr1 +  "WHERE mst.prefectures LIKE $1 ";
-  queryStr1     = queryStr1 +     "OR mst.municipality LIKE $1 ";
-  queryStr1     = queryStr1 +     "OR mst.district LIKE $1 ";
-  client.query(queryStr1, ["%" + word + "%"]).then(res => {
-      if (res.rows.length > 0) {
-          if (res.rows[0].cnt != null && res.rows[0].cnt > 0) {
-              // 地域データが存在する場合
-              isArea = true;
-          }
-      }
-          // キーワード判定
-          let queryStr2 =             "SELECT ";
-          queryStr2     = queryStr2 +     "COUNT(mst.*) AS cnt ";
-          queryStr2     = queryStr2 +   "FROM m_category mst ";
-          queryStr2     = queryStr2 +  "WHERE mst.category_name LIKE $1 ";
-          client.query(queryStr2, ["%" + word + "%"]).then(res => {
-              if (res.cnt > 0) {
-                  if (res.rows[0].cnt != null && res.rows[0].cnt > 0) {
-                      // カテゴリデータが存在する場合
-                      isKeyword = true;
-                  }
-              }
-              if (isArea) {
-                  // 単語が地域の場合
-                  area = word;
-              }
-
-              if (isKeyword) {
-                  // キーワードが地域の場合
-                  keyword = word;
-              }
-          }).catch(e => {
-              console.log("データ取得に失敗しました");
-              console.error(e);
-              client.end();
-          })
-      // client.end();
-  }).catch(e => {
-      console.log("データ取得に失敗しました");
-      console.error(e);
-      client.end();
-  })
-
-  result.isArea = isArea;
-  result.isKeyword = isKeyword;
-  result.area = area;
-  result.keyword = keyword;
-
-  return result;
+    })
 }
